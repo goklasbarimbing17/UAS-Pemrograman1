7f3d48af597472c1b0b60f4d0908e77341c3751b
GoklasBarimbing <36942871+goklasbarimbing17@users.noreply.github.com>
2018-04-17 19:18:08 +0700

    Add files via upload

---
+++ Uas.py
@@ -0,0 +1,19 @@
+import Goklas.a
+import Goklas.b
+import Goklas.c
+import Goklas.d
+import Goklas.e
+import Goklas.f
+
+
+print Goklas.a.mhs()
+
+print Goklas.b.random()
+
+print Goklas.c.lisst()
+
+print Goklas.d.tup()
+
+print Goklas.e.dic()
+
+print Goklas.f.kal()
